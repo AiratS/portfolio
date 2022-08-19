2ae0829d0ceebb916322cc5001442c3e81251ab2
Airat Sirazov <sirazovairat@yandex.ru>
2022-08-19 20:33:59 +0300

    feat: add TheAboutMe.vue and TheQualities.vue components

---
+++ resources/js/components/TheAboutMe.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="app-page-block">
+    <div class="app-page-block__bg">
+      About
+    </div>
+    <div class="app-page-block__main">
+      <div class="app-page-block__title">
+        About me
+      </div>
+      <div class="app-page-block__content the-about-me-content">
+        <div class="the-about-me-content__photo">
+          <img
+            class="the-about-me-content__img"
+            src="../../assets/images/the-about-me-photo.jpeg"
+            alt="the-about-me-photo"
+          />
+        </div>
+        <div class="the-about-me-content__personal">
+          <div class="the-about-me-content__specialty">
+            UI/UX Designer & Web Developer
+          </div>
+          <div class="the-about-me-content__info">
+            Tempor eos dolore amet tempor dolor tempor.
+            Dolore ea magna sit amet dolor eirmod.
+            Eos ipsum est tempor dolor. Clita lorem kasd sed
+            ea lorem diam ea lorem eirmod duo sit ipsum. Amet
+            dolor stet lorem diam dolor justo et dolor dolor dolor
+          </div>
+          <div class="the-about-me-content__qualities">
+            <the-qualities :qualities="qualities" />
+          </div>
+          <div class="the-about-me-content__buttons">
+            <app-button variant="white-green">Hire me</app-button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TheQualities from "./TheQualities";
+import AppButton from "./AppButton";
+
+export default {
+  name: "TheAboutMe",
+  components: {
+    AppButton,
+    TheQualities,
+  },
+  data() {
+    return {
+      qualities: [
+        {
+          type: 'Name',
+          value: 'Kate'
+        },
+        {
+          type: 'Name',
+          value: 'Kate'
+        },
+        {
+          type: 'Name',
+          value: 'Kate'
+        },
+        {
+          type: 'Name',
+          value: 'Kate'
+        },
+      ]
+    };
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/components/the-about-me";
+</style>
---
+++ resources/js/components/TheQualities.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="the-qualities">
+    <div
+      v-for="quality in qualities"
+      :key="quality.type"
+      class="the-qualities__quality"
+    >
+      <span class="the-qualities__type">
+        {{ quality.type }}:
+      </span>
+      <span class="the-qualities__value">
+        {{ quality.value }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheQualities",
+  props: {
+    qualities: {
+      type: Array,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/abstract/colors";
+
+.the-qualities {
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+
+  .the-qualities__quality {
+    width: 50%;
+    font-size: 16px;
+    font-weight: 400;
+    margin-bottom: 20px;
+
+    .the-qualities__type {
+      color: $app-black;
+    }
+
+    .the-qualities__value {
+      color: $app-dark-grey;
+    }
+  }
+}
+</style>
