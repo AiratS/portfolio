febbfab3cfcbff05487151fc3ae2881815cdab47
Airat Sirazov <sirazovairat@yandex.ru>
2022-08-19 19:32:35 +0300

    feat: add TheProfile.vue component

--- resources/js/components/TheProfile.vue
+++ resources/js/components/TheProfile.vue
@@ -1,30 +1,35 @@
 <template>
   <div class="the-profile">
-    <div class="the-profile__avatar">
-      <img class="the-profile__avatar-img" src="../../assets/images/avatar.jpeg" alt="avatar">
-    </div>
-    <div class="the-profile__info">
-      <div class="the-profile__i-am">
-        {{ profile.iAm }}
+    <div class="the-profile__content">
+      <div class="the-profile__avatar">
+        <img class="the-profile__avatar-img" src="../../assets/images/avatar.jpeg" alt="avatar">
       </div>
-      <div class="the-profile__full-name">
-        {{ profile.fullName }}
-      </div>
-      <div class="the-profile__specialty">
-        {{ profile.specialty }}
-      </div>
-      <div class="the-profile__cv">
-        <a href="{{ profile.cvUrl }}" target="_blank">
-          {{ profile.cvLabel }}
-        </a>
+      <div class="the-profile__info">
+        <div class="the-profile__i-am">
+          {{ profile.iAm }}
+        </div>
+        <div class="the-profile__full-name">
+          {{ profile.fullName }}
+        </div>
+        <div class="the-profile__specialty">
+          <the-text-type :text="profile.specialty"></the-text-type>
+        </div>
+        <div class="the-profile__cv">
+          <app-button>
+            {{ profile.cvLabel }}
+          </app-button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TheTextType from "./TheTextType";
+import AppButton from "./AppButton";
 export default {
   name: "TheProfile",
+  components: {AppButton, TheTextType},
   data() {
     return {
       profile: {
---
+++ resources/js/components/TheTextType.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="the-text-type">
+    {{ text }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TheTextType",
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.the-text-type {
+  font-size: 40px;
+  font-weight: 300;
+  color: white;
+}
+</style>
