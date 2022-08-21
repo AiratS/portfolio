a0a19f5ccabcc5b9220f187b15b0be49eaaa43f1
Airat Sirazov <sirazovairat@yandex.ru>
2022-08-22 00:06:02 +0300

    feat: add TheContactMe.vue, AppTextarea.vue components

---
+++ resources/js/components/AppTextarea.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="app-textarea">
+    <textarea
+      class="app-textarea__textarea"
+      :value="modelValue"
+      :placeholder="placeholder"
+      @input="handleInput"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppTextarea",
+  props: {
+    modelValue: null,
+    placeholder: {
+      type: String,
+      required: false,
+    },
+  },
+  methods: {
+    handleInput(event) {
+      this.$emit('update:modelValue', event.target.value);
+    }
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/abstract/colors";
+
+.app-textarea {
+  width: 100%;
+
+  .app-textarea__textarea {
+    width: 100%;
+    height: 155px;
+    padding: 20px 26px;
+    resize: none;
+    border: 1px solid $app-light-grey;
+    border-radius: 30px;
+    font-size: 16px;
+    font-weight: 400;
+    outline-color: $app-green;
+  }
+}
+</style>
---
+++ resources/js/components/TheContactMe.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="app-page-block">
+    <div class="app-page-block__bg">
+      Contact
+    </div>
+    <div class="app-page-block__main">
+      <div class="app-page-block__title">
+        Contact me
+      </div>
+      <div class="app-page-block__content the-contact-me-content">
+        <div class="the-about-me-content__form-control the-contact-me-content__name-email">
+          <app-input
+            class="the-contact-me-content__name"
+            placeholder="Name"
+            v-model="name"
+          />
+          <app-input
+            class="the-contact-me-content__email"
+            placeholder="Email"
+            v-model="email"
+          />
+        </div>
+        <app-input
+          class="the-about-me-content__form-control"
+          placeholder="Subject"
+          v-model="subject"
+        />
+        <app-textarea
+          class="the-about-me-content__form-control"
+          placeholder="Subject"
+          v-model="message"
+        />
+        <app-button
+          class="the-about-me-content__button"
+          variant="white-green"
+        >
+          Send message
+        </app-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import AppInput from "./AppInput";
+import AppTextarea from "./AppTextarea";
+import AppButton from "./AppButton";
+
+export default {
+  name: "TheContactMe",
+  components: {
+    AppButton,
+    AppInput,
+    AppTextarea,
+  },
+  data() {
+    return {
+      name: null,
+      email: null,
+      subject: null,
+      message: null,
+    };
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.the-contact-me-content {
+  width: 728px;
+  margin-left: auto;
+  margin-right: auto;
+
+  .the-about-me-content__form-control {
+    margin-bottom: 15px;
+  }
+
+  .the-contact-me-content__name-email {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+
+    .the-contact-me-content__name {
+      width: 45%;
+      margin-right: 3px;
+    }
+
+    .the-contact-me-content__email {
+      width: 45%;
+      margin-left: 3px;
+    }
+  }
+
+  .the-about-me-content__button {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+</style>
