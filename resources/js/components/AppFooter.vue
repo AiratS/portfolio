da6b88c9cda13effc7a8ce654bf4572e3a577608
Airat Sirazov <sirazovairat@yandex.ru>
2022-08-22 00:48:04 +0300

    feat: add AppFooter.vue and HomeView.vue components

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="app-footer">
+    <div class="app-footer__social">
+      <div
+        v-for="(social, index) in socials"
+        :key="index"
+        class="app-footer__social-link"
+      >
+        <a :href="social.link"></a>
+      </div>
+    </div>
+    <div class="app-footer__links">
+        <a
+          v-for="(link, index) in links"
+          :key="index"
+          class="app-footer__link"
+          href="#"
+        >
+          {{ link.text }}
+        </a>
+    </div>
+    <div class="app-footer__rights">
+      © Domain Name. All Rights Reserved. Designed by HTML Codex
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+  data() {
+    return {
+      socials: [
+        {
+          icon: '',
+          link: '#',
+        },
+        {
+          icon: '',
+          link: '#',
+        },
+        {
+          icon: '',
+          link: '#',
+        },
+      ],
+      links: [
+        {
+          text: 'Privacy',
+        },
+        {
+          text: 'Privacy',
+        },
+        {
+          text: 'Privacy',
+        },
+        {
+          text: 'Privacy',
+        },
+      ],
+    };
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/abstract/colors";
+
+.app-footer {
+  width: 100%;
+  margin-top: 90px;
+  padding-top: 96px;
+  padding-bottom: 96px;
+  background-color: $app-green;
+
+  .app-footer__social {
+    display: flex;
+    gap: 8px;
+    width: fit-content;
+    margin-left: auto;
+    margin-right: auto;
+
+    .app-footer__social-link {
+      width: 45px;
+      height: 45px;
+      background-color: white;
+      border-radius: 45px;
+    }
+  }
+
+  .app-footer__links {
+    text-align: center;
+    margin-top: 30px;
+
+    .app-footer__link {
+      padding-left: 15px;
+      padding-right: 15px;
+      border-right: 2px solid white;
+      font-size: 16px;
+      font-weight: 400;
+      color: white;
+      text-decoration: none;
+    }
+
+    .app-footer__link:first-child {
+      padding-left: 0;
+    }
+
+    .app-footer__link:last-child {
+      padding-right: 0;
+      border-right: none !important;
+    }
+  }
+
+  .app-footer__rights {
+    margin-top: 25px;
+    text-align: center;
+    font-size: 16px;
+    font-weight: 400;
+    color: white;
+  }
+}
+</style>
--- resources/js/views/HomeView.vue
+++ resources/js/views/HomeView.vue
@@ -1,15 +1,28 @@
 <template>
   <div class="home-view">
-    home view
+    <the-profile/>
+    <the-about-me/>
+    <the-education-experience/>
+    <the-contact-me/>
+    <app-footer/>
   </div>
 </template>
 
 <script>
+import TheProfile from "../components/TheProfile";
+import TheAboutMe from "../components/TheAboutMe";
+import TheEducationExperience from "../components/TheEducationExperience";
+import TheContactMe from "../components/TheContactMe";
+import AppFooter from "../components/AppFooter";
+
 export default {
-  name: "HomeView"
+  name: "HomeView",
+  components: {
+    AppFooter,
+    TheContactMe,
+    TheEducationExperience,
+    TheAboutMe,
+    TheProfile,
+  },
 }
 </script>
-
-<style scoped>
-
-</style>
