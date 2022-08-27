7c92f161cf4e9405d7ad4a0e35a9f3f032c1a337
Airat Sirazov <sirazovairat@yandex.ru>
2022-08-27 19:20:15 +0300

    feat: add adaptive version of AppHeader.vue

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="app-header" :class="{ 'app-header_visible': isVisible }">
+  <div class="app-header" :class="headerModifiers">
     <div class="app-header__content">
       <div class="app-header__logo">
         <a href="/" class="app-header__logo-link">
@@ -27,6 +27,14 @@
       <div class="app-header__github">
         <app-button variant="white-green">GitHub</app-button>
       </div>
+      <div
+        class="app-header__menu"
+        @click="onMenuClicked"
+      >
+        <div class="app-header__menu-line"></div>
+        <div class="app-header__menu-line"></div>
+        <div class="app-header__menu-line"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -36,9 +44,11 @@ import AppButton from "./AppButton";
 
 export default {
   name: "AppHeader",
+
   components: {
     AppButton,
   },
+
   props: {
     navItems: {
       type: Array,
@@ -67,6 +77,7 @@ export default {
   data() {
     return {
       isVisible: false,
+      isExpanded: false,
       clickedNavItemText: null,
     };
   },
@@ -76,10 +87,19 @@ export default {
 
     document.addEventListener('scroll', () => {
       this.isVisible = halfHeight < window.scrollY;
+      if (!this.isVisible) {
+        this.isExpanded = false;
+      }
     });
   },
 
   computed: {
+    headerModifiers() {
+      return {
+        'app-header_visible': this.isVisible,
+        'app-header_expanded': this.isExpanded,
+      };
+    },
     navItemsProcessed() {
       return this.navItems.map((item) => {
         return {
@@ -94,6 +114,10 @@ export default {
   methods: {
     onNavItemClicked(text) {
       this.clickedNavItemText = text;
+      this.isExpanded = false;
+    },
+    onMenuClicked() {
+      this.isExpanded = !this.isExpanded;
     },
     _getPageHeight() {
       const body = document.body;
@@ -148,6 +172,7 @@ export default {
     .app-header__logo {
       font-size: 40px;
       font-weight: 900;
+      margin-left: 48px;
 
       .app-header__logo-link {
         text-decoration: none;
@@ -183,11 +208,94 @@ export default {
         }
       }
     }
+
+    .app-header__github {
+      margin-right: 48px;
+    }
+
+    .app-header__menu {
+      display: none;
+    }
+  }
+}
+
+@media (max-width: 1450px) {
+  .app-header__content {
+    width: 100% !important;
+  }
+}
+
+@media (max-width: 960px) {
+  .app-header {
+    transition: height .06s ease-in !important;
+
+    .app-header__content {
+      position: relative;
+      display: block;
+
+      .app-header__logo {
+        margin-left: 15px;
+      }
+
+      .app-header__nav-block {
+        display: none;
+
+        .app-header__nav {
+          flex-direction: column;
+          padding-left: 10px;
+          margin-bottom: 2px;
+
+          .app-header__nav-item {
+            margin-bottom: 25px;
+          }
+        }
+      }
+
+      .app-header__github {
+        display: none;
+      }
+
+      .app-header__menu {
+        position: absolute;
+        display: block;
+        width: fit-content;
+        height: fit-content;
+        top: 13px;
+        right: 0;
+        padding: 10px 15px 4px 15px;;
+        margin-right: 10px;
+        border: 1px solid $app-grey;
+        border-radius: 4px;
+        cursor: pointer;
+
+        .app-header__menu-line {
+          width: 26px;
+          height: 2px;
+          background-color: $app-dark-grey;
+          border-radius: 2px;
+          margin-bottom: 6px;
+        }
+      }
+
+      .app-header__menu:hover {
+        border-width: 1px;
+        border-color: $app-blue;
+      }
+    }
   }
 }
 
 .app-header_visible {
   display: block;
   opacity: 1;
+}
+
+.app-header_expanded {
+  height: auto;
+  max-height: 500px;
+
+  .app-header__nav-block {
+    display: block !important;
+  }
 }
 </style>
