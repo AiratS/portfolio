4c92a8ac0835ddf3f098783ca5344a80fdbdcb12
Airat Sirazov <sirazovairat@yandex.ru>
2022-08-30 19:37:01 +0300

    refactor: make common layout for error pages

---
+++ resources/js/layouts/AppErrorLayout.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="app-error-layout">
+    <app-header always-visible/>
+    <div class="app-error-layout__content">
+      <div class="app-error-layout__left">
+        <div class="app-error-layout__http-code">
+          <slot name="http-code"></slot>
+        </div>
+        <div class="app-error-layout__text">
+          <slot name="text"></slot>
+        </div>
+        <div class="app-error-layout__try">
+          <slot></slot>
+        </div>
+      </div>
+      <div class="app-error-layout__right">
+        <slot name="picture">
+          <img
+            class="app-error-layout__picture"
+            src="../../assets/images/funny-cat.jpg"
+            alt="funny-cat"
+          >
+        </slot>
+      </div>
+    </div>
+    <app-footer/>
+  </div>
+</template>
+
+<script>
+import AppHeader from "../components/AppHeader";
+import AppFooter from "../components/AppFooter";
+
+export default {
+  name: "AppErrorLayout",
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../styles/abstract/variables";
+@import "../../styles/abstract/colors";
+
+.app-error-layout__content {
+  display: flex;
+  width: $app-home-width;
+  height: 65vh;
+  justify-content: space-between;
+  padding-top: 10%;
+  padding-bottom: 50px;
+  margin-left: auto;
+  margin-right: auto;
+
+  .app-error-layout__left {
+    width: 50%;
+
+    .app-error-layout__http-code {
+      font-size: 40px;
+      font-weight: 700;
+      color: $app-green;
+    }
+
+    .app-error-layout__text {
+      margin-top: 20px;
+      font-size: 48px;
+      font-weight: 600;
+      line-height: 56px;
+    }
+
+    .app-error-layout__try {
+      margin-top: 25px;
+      font-size: 18px;
+      font-weight: 300;
+      line-height: 24px;
+    }
+  }
+
+  .app-error-layout__right {
+    overflow: hidden;
+
+    .app-error-layout__picture {
+      width: 460px;
+    }
+  }
+}
+
+@media (max-width: 1200px) {
+  .app-error-layout__content {
+    width: $app-home-width-lg;
+  }
+}
+
+@media (max-width: 992px) {
+  .app-error-layout__content {
+    width: $app-home-width-md;
+    padding-top: 100px;
+
+    .app-error-layout__left {
+      width: 100%;
+    }
+
+    .app-error-layout__right {
+      display: none;
+    }
+  }
+}
+
+@media (max-width: 768px) {
+  .app-error-layout__content {
+    width: 100%;
+    text-align: center;
+
+    .app-error-layout__left {
+      .app-error-layout__text {
+        font-size: 32px;
+      }
+    }
+  }
+}
+</style>
--- resources/js/views/NotFoundView.vue
+++ resources/js/views/NotFoundView.vue
@@ -1,132 +1,36 @@
 <template>
-  <div class="not-found-view">
-    <app-header always-visible/>
+  <app-error-layout>
+    <template #http-code>
+      404
+    </template>
+    <template #text>
+      Уупс! Это не то, что вы искали
+    </template>
 
-    <div class="not-found-view__content">
-      <div class="not-found-view__left">
-        <div class="not-found-view__http-code">
-          404
-        </div>
-        <div class="not-found-view__text">
-          Уупс! Это не то, что вы искали
-        </div>
-        <div class="not-found-view__try-home">
-          Испытайте свою удачу, вернувшись на
-          <router-link class="not-found-view__home-link" :to="{ name: 'home' }">главную страницу</router-link>.
-        </div>
-      </div>
-      <div class="not-found-view__right">
-        <img
-          class="not-found-view__picture"
-          src="../../assets/images/funny-cat.jpg"
-          alt="funny-cat"
-        >
-      </div>
-    </div>
-
-    <app-footer/>
-  </div>
+    Испытайте свою удачу, вернувшись на
+    <router-link class="home-link" :to="{ name: 'home' }">
+      главную страницу
+    </router-link>.
+  </app-error-layout>
 </template>
 
 <script>
-import AppHeader from "../components/AppHeader";
-import AppFooter from "../components/AppFooter";
+import AppErrorLayout from "../layouts/AppErrorLayout";
 
 export default {
   name: "NotFoundView",
   components: {
-    AppHeader,
-    AppFooter,
+    AppErrorLayout,
   },
 }
 </script>
 
 <style lang="scss" scoped>
-@import "../../styles/abstract/variables";
-@import "../../styles/abstract/colors";
+.home-link {
+  text-decoration: none;
 
-.not-found-view__content {
-  display: flex;
-  width: $app-home-width;
-  height: 65vh;
-  justify-content: space-between;
-  padding-top: 10%;
-  padding-bottom: 50px;
-  margin-left: auto;
-  margin-right: auto;
-
-  .not-found-view__left {
-    width: 50%;
-
-    .not-found-view__http-code {
-      font-size: 40px;
-      font-weight: 700;
-      color: $app-green;
-    }
-
-    .not-found-view__text {
-      margin-top: 20px;
-      font-size: 48px;
-      font-weight: 600;
-      line-height: 56px;
-    }
-
-    .not-found-view__try-home {
-      margin-top: 25px;
-      font-size: 18px;
-      font-weight: 300;
-      line-height: 24px;
-
-      .not-found-view__home-link {
-        text-decoration: none;
-      }
-    }
-  }
-
-  .not-found-view__right {
-    overflow: hidden;
-
-    .not-found-view__picture {
-      width: 460px;
-    }
-  }
-}
-
-@media (max-width: 1200px) {
-  .not-found-view__content {
-    width: $app-home-width-lg;
-  }
-}
-
-@media (max-width: 992px) {
-  .not-found-view__content {
-    width: $app-home-width-md;
-    padding-top: 100px;
-
-    .not-found-view__left {
-      width: 100%;
-    }
-
-    .not-found-view__right {
-      display: none;
-    }
-  }
-}
-
-@media (max-width: 768px) {
-  .not-found-view__content {
-    width: 100%;
-    text-align: center;
-
-    .not-found-view__left {
-      .not-found-view__text {
-        font-size: 32px;
-      }
-
-      .not-found-view__home-link {
-        display: block;
-      }
-    }
+  @media (max-width: 768px) {
+    display: block;
   }
 }
 </style>
